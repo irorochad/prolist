cc7c8a1bb240f23ba09b1241ef992a646319cd17
irorochad <wisdomchadere@gmail.com>
2022-09-08 14:11:34 +0100

    first first of the project header done

--- public/project.php
+++ public/project.php
@@ -2,8 +2,34 @@
 <?php include "./includes/navbar.inc.php" ?>
 
 
-<div class="container flex flex-col md:flex-row items-center px-6 mx-auto pt-20 space-y-0 md:space-y-0">
+<div class="mt-20 mb-20 px-4">
+    <!-- Main Project Header -->
+    <div class=" grid grid-cols-2   items-center w-full">
+        <!-- Left Header -->
+        <div class="md:w-1/2">
+            <div class="flex justify-start gap-4 ">
+                <div class="">
+                    <img src="https://s2.coinmarketcap.com/static/img/coins/64x64/1839.png" alt="Logo" />
+                </div>
+                <div class="flex flex-col">
+                    <h1>Binance</h1>
+                    <span class="flex flex-row gap-4">
+                        <h3>Category</h3>
+                        <p>Save</p>
+                    </span>
+                </div>
+            </div>
+        </div> <!-- End Left Header -->
 
+
+        <!-- Right Header -->
+        <div class="flex justify-end">
+        <!-- <p class="text-gray-800 dark:text-gray-200 ">Visit Site</p> -->
+        <a href="./register.php" class="text-sm px-4 py-2 rounded-lg baseline font-semibold text-white bg-brandBlue">Visit Website</a>
+
+        </div> <!-- End Right Header -->
+    </div>
+    <!-- End Project Header -->
 </div>
 
 <?php include "./includes/footer.inc.php" ?>
