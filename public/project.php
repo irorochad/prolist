379c2732916a16084c06fba24d4c3a1d18cb38f0
irorochad <wisdomchadere@gmail.com>
2022-09-09 01:32:53 +0100

    project header done

--- public/project.php
+++ public/project.php
@@ -7,15 +7,18 @@
     <div class=" grid grid-cols-2  items-center w-full">
         <!-- Left Header -->
         <div class="md:w-1/2">
-            <div class="flex justify-start gap-4 ">
+            <div class="flex justify-start items-center ">
                 <div class="">
-                    <img src="https://s2.coinmarketcap.com/static/img/coins/64x64/1839.png" alt="Logo" />
+                    <img src="https://s2.coinmarketcap.com/static/img/coins/64x64/8616.png" class="md:w-full" alt="Logo" />
                 </div>
-                <div class="flex flex-col">
-                    <h1 class="text-4xl md:text-5xl font-bold  text-brandBlue dark:to-blue-700 md:text-left">Binance</h1>
-                    <span class="flex flex-row gap-4">
+                <div class="flex flex-col pl-4 md:pl-4 text-gray-800 dark:text-gray-100">
+                    <h1 class="md:text-4xl font-bold">Binance</h1>
+                    <span class="flex flex-row">
                         <h3>Category</h3>
-                        <p>Save</p>
+                        <p class="ml-2 text-blue-600 dark:text-blue-500"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" />
+                            </svg>
+                        </p>
                     </span>
                 </div>
             </div>
@@ -24,8 +27,13 @@
 
         <!-- Right Header -->
         <div class="flex justify-end">
-        <!-- <p class="text-gray-800 dark:text-gray-200 ">Visit Site</p> -->
-        <a href="./register.php" class="text-sm px-4 py-2 rounded-lg baseline font-semibold text-white bg-brandBlue">Visit Website</a>
+            <!-- <p class="text-gray-800 dark:text-gray-200 ">Visit Site</p> -->
+            <a href="#" class="inline-flex items-center py-2 px-3 text-md font-semibold text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 dark:bg-blue-600 ">
+                Website
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 ml-2">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M13.19 8.688a4.5 4.5 0 011.242 7.244l-4.5 4.5a4.5 4.5 0 01-6.364-6.364l1.757-1.757m13.35-.622l1.757-1.757a4.5 4.5 0 00-6.364-6.364l-4.5 4.5a4.5 4.5 0 001.242 7.244" />
+                </svg>
+            </a>
 
         </div> <!-- End Right Header -->
     </div>
