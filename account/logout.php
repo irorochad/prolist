baafc96c1632f92db7057e800d0becae18cc584a
irorochad <wisdomchadere@gmail.com>
2022-09-25 19:14:33 +0100

    changed the logout link to a post method

--- account/logout.php
+++ account/logout.php
@@ -1,7 +1,9 @@
 <?php ob_start();
-     session_start();
-    $_SESSION['user_id'] = null;
-    $_SESSION['user_name'] = null;
-    $_SESSION['user_password'] = null;
-    $_SESSION['user_email'] = null;
-    header("Location: ../");
+session_start();
+if (isset($_POST['logout'])) {
+     $_SESSION['user_id'] = null;
+     $_SESSION['user_name'] = null;
+     $_SESSION['user_password'] = null;
+     $_SESSION['user_email'] = null;
+     header("Location: ../");
+}
