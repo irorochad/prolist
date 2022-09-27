da6ea5f7ad229ef45cd0661d462ab1e31f3b7f18
irorochad <wisdomchadere@gmail.com>
2022-09-27 01:08:00 +0100

    added the isset session to the mobile nav to show the login/register link or the dashboard link

--- includes/navbar.inc.php
+++ includes/navbar.inc.php
@@ -92,7 +92,7 @@
       <a href="#" class="text-gray-800 dark:text-gray-200">Resources</a>
 
       <!-- If a session is set, then the dashboard link is shown, else the login and register btn is shown -->
-      <?php if ($_SESSION['user_name']) : ?>
+      <?php if (isset($_SESSION['user_name'])) : ?>
           <a href="/prolist/account" class="text-blue-800"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
               <path stroke-linecap="round" stroke-linejoin="round" d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z" />
             </svg>
